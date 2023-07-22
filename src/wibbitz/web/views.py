2fc575405dc8f2e1cbe5d433362172f78f153e5f
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-22 11:04:04 +0530

    Display customers on template

--- src/wibbitz/web/views.py
+++ src/wibbitz/web/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from web.models import Customer
 
 
 def index(request):
-    context={}
+    customer = Customer.objects.all()
+    context={
+        "customers" : customer
+    }
     return render(request,"index.html",context=context)
