2280b75603a9bca862c17adf84c9b7e46581771a
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-22 11:33:06 +0530

    Subscribe form submission

--- src/wibbitz/web/urls.py
+++ src/wibbitz/web/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from web.views import index
+from web.views import index,subscribe
 
 
 app_name="web"
 
 urlpatterns = [
-    path("",index,name="index")
+    path("",index,name="index"),
+    path("subscribe/", subscribe, name="subscribe")
 ]
--- src/wibbitz/web/views.py
+++ src/wibbitz/web/views.py
@@ -1,5 +1,8 @@
+import json
+
 from django.shortcuts import render
-from web.models import Customer
+from web.models import Customer,Subscribe
+from django.http import HttpResponse
 
 
 def index(request):
@@ -8,3 +11,27 @@ def index(request):
         "customers" : customer
     }
     return render(request,"index.html",context=context)
+
+
+def subscribe(request):
+    email = request.POST.get("email")
+    
+    if not Subscribe.objects.filter(email=email).exists():
+        
+        Subscribe.objects.create(
+          email = email
+        )
+
+        response_data={
+            "status" : "success",
+            "title" : "Successfully Registered",
+            "message" : "You Subscribed to our newsletter successfully"
+        }
+    else:
+        response_data={
+            "status" : "error",
+            "title" : "You are already subscribed",
+            "message" : "You are already member. No need to register again"
+        }
+
+    return HttpResponse(json.dumps(response_data),content_type="application/javascript")
