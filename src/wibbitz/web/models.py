473b5f5187f7a60ba9070b0c1f69f0c3f5700a36
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-22 10:34:56 +0530

    created subscribe and customer models

--- src/wibbitz/web/models.py
+++ src/wibbitz/web/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Subscribe(models.Model):
+    email = models.EmailField()
+
+    class Meta:
+        db_table = "web_subscribe"
+        ordering = ["-id"]
+    
+    def __str__(self):
+        return self.email
+    
+
+
+class Customer(models.Model):
+    image = models.FileField(upload_to="customers/")
+
+    class Meta:
+        db_table = "web_customer"
+        ordering = ["-id"]
+    
+    def __str__(self):
+        return str(self.id)
